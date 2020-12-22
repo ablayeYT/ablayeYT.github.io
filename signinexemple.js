29b3ce9b07770680538db254748f7cc03c03f56d
baztoul76 <bazlefebvre@gmail.com>
2020-12-22 16:01:14 +0100

    j'ai mis le fichier

---
+++ signinexemple.js
@@ -0,0 +1,89 @@
+<!-- Sign in templates fait par Wharkk -->
+<!DOCTYPE html>
+<html lang="fr">
+  <head> <!-- meta et titre de la page -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.79.0">
+    <title>Inscrivez-vous</title>
+
+    <link rel="canonical" href="https:///siteeco/getbootstrap.com/docs/5.0/examples/sign-in/">
+
+    <!-- Css touche pas a la commande si tu veut charger le css -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+      <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+  </head>
+  
+  <header> <!-- Menu du haut de page tu peut rajouter des boutons stv -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Titre</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item active">
+            <a class="nav-link" aria-current="page" href="file:///F://siteeco/carousel/index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="file:///F://siteeco/L'%C3%A9quipe%20carrousel/index.html">Bouton</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Bouton desactivé</a>
+          </li>
+        </ul>
+  </header>
+
+  <body class="text-center">
+    
+<main class="form-signin"> <!-- millieu de page tu peut rajouter des label/input stv-->
+  <form>
+    <h1 class="h3 mb-3 fw-normal">Inscrivez-vous</h1>
+    <label for="inputName" class="visually-hidden">Nom</label>
+    <input type="Name" id="inputName" class="form-control" placeholder="Nom" required autofocus>
+    <label for="inputName" class="visually-hidden">Prénom</label>
+    <input type="Name" id="inputName" class="form-control" placeholder="Prénom" required autofocus>
+    <label for="inputEmail" class="visually-hidden">Adresse email</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="Adresse email" required autofocus>
+    <label for="inputPassword" class="visually-hidden">Mot de passe</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Se souvenir de moi
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">S'inscrire</button>
+  </form>
+
+  <hr class="featurette-divider"> <!-- Menu bas de page -->
+    <footer class="container">
+    <p>&copy; 2020 Company, Inc. &middot; <a href="lelientapolitiquedeconfidentialité">Politique de confidentialité</a> &middot; <a href="leliendetesconditionsgénérales">Conditions générales</a></p>
+  </footer>
+</main>
+
+
+    
+  </body>
+</html>
