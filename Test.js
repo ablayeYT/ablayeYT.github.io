cd82e32c4a4fa28f3eb210951a20a085ef69bfd5
baztoul76 <bazlefebvre@gmail.com>
2020-12-22 15:50:16 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,12 +1,12 @@
-document.write("xD")
-document.write("CRAFTIUM")
-document.write("EN DEV")
-document.write("PLAYER : 3")
+document.write("xD");
+document.write("CRAFTIUM");
+document.write("EN DEV");
+document.write("PLAYER : 3");
 
 vrai = true;
 
 if (vrai) {
-  console.log ("CRAFTIUM")
-  console.log ("EN DEV")
+  console.log ("CRAFTIUM");
+  console.log ("EN DEV");
   console.log ("PLAYER : 3");
-  {
+}
