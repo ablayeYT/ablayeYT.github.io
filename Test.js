0705ff45badabb90520fdecfde2f5328007b210d
baztoul76 <bazlefebvre@gmail.com>
2020-12-24 11:59:32 +0100

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,4 +1,20 @@
-let playerNumber = 3;
+let playerNumber = null;
+
+const request = new XMLHttpRequest();
+
+request.onreadystatechange = () => {
+  if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+    let response = JSON.parse(this.responseText);
+    playerNumber = response.playernumber;
+  }
+};
+
+request.open("GET", "https://mon.serveur.mc/json/infos/players");
+request.send();
+
+// comme y'a pas de site on va mettre à 3 lol
+playerNumber = 3;
+
 
 document.write("xD <br />");
 document.write("CRAFTIUM <br />");
