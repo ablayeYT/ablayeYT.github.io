a96befecfb247a2a25c711ebe1237fa06b8a8e7c
baztoul76 <bazlefebvre@gmail.com>
2021-05-26 19:50:54 +0200

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,17 +1,16 @@
 let e = document.getElementById("infos");
 
-const getOnlinePlayers = () => {
-  fetch("https://ablayeyt.github.io/json/infos/players").then(res => {
-    if (res.ok) {
-      return res.json();
-    }
-  }).then(value => {
-    document.getElementById("infos").innerHTML = "NARTIUM <br />EN DEV <br />JOUEURS : " + value.player_count + "<br/>";
-  }).catch(err => {
-    document.getElementById("infos").innerHTML = "NARTIUM <br />EN DEV <br />JOUEURS : " + "nombre inconnu" + "<br/>";
-    console.log("erreur : " + err);
-  });
-}
+fetch("https://ablayeyt.github.io/json/infos/players").then(res => {
+  if (res.ok) {
+    return res.json();
+  }
+}).then(value => {
+  document.getElementById("infos").innerHTML = "NARTIUM <br />EN DEV <br />JOUEURS : " + value.player_count + "<br/>";
+}).catch(err => {
+  document.getElementById("infos").innerHTML = "NARTIUM <br />EN DEV <br />JOUEURS : " + "nombre inconnu" + "<br/>";
+  console.log("erreur : " + err);
+});
+
 
 vrai = true;
 
