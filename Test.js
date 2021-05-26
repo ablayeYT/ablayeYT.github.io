0c51c37cdfaed83c68def7e20629029d5c643ec6
baztoul76 <bazlefebvre@gmail.com>
2021-05-26 19:38:49 +0200

    Update Test.js

--- Test.js
+++ Test.js
@@ -3,7 +3,7 @@ let e = document.getElementById("infos");
 const getOnlinePlayers = () => {
   fetch("https://ablayeyt.github.io/json/infos/players").then(res => {
     if (res.ok) {
-      return res;
+      return res.json();
     }
   }).then(value => {
     document.getElementById("infos").innerHTML = "NARTIUM <br />EN DEV <br />JOUEURS : " + value.player_count + "<br/>";
