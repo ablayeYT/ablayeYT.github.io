04924de81096aeb2a9f57d457866a8d12e3fa26f
baztoul76 <bazlefebvre@gmail.com>
2021-09-03 17:30:25 +0200

    sécurisation et suppression s'erreurs

--- Cookie.php
+++ Cookie.php
@@ -1,5 +1,5 @@
 <?php
 $cookie_name = "user";
 $cookie_value = "Test";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30,"/");
+setcookie($cookie_name, $cookie_value, time() + 30*24*60*60, null, null, false, true;
 // ah et fais gaffe le PHP marche pas sur github pages
