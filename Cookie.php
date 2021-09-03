a5eb8fcc2f7553088546cc3c01da7e033f9c8574
baztoul76 <bazlefebvre@gmail.com>
2021-09-03 17:19:21 +0200

    Update Cookie.php

--- Cookie.php
+++ Cookie.php
@@ -1,4 +1,4 @@
 <?php
 $cookie_name = "user";
 $cookie_value = "Test";
-setcookie( $cookie_name,$cookie_value time() + (86400 * 30,"/") ;
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30,"/");
